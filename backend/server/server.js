4d37c1ea08afa05e1edfff01df42050f2aaf5328
Abdullah Mohammed <ysjsjdkd4@gmail.com>
2023-07-27 14:57:44 +0500

    Adding User, Login JWT API

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,13 +1,26 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
 const app = express();
-const port = 3000;
+app.use(bodyParser.json());
+const port = 9000;
 
-// Route for the root URL
-app.get('/', (req, res) => {
-  res.send('Hello, this is a server!');
-});
+const mongodbUrl = 'mongodb+srv://abdullahmohammed:Hel5Mb4HkPe4PffC@cluster0.2rsepks.mongodb.net/';
+mongoose.connect(mongodbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
-// Start the server
+const authenticationRoutes = require('./routes/authentication');
+
+app.use('/auth', authenticationRoutes);
+
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
